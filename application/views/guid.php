6a8fa589ae23332b341a860ef9f8fe8dca52fce2
chenbo <1264339504@qq.com>
2016-08-13 19:36:58 +0800

    buju

--- application/views/guid.php
+++ application/views/guid.php
@@ -2,16 +2,16 @@
     <div class="alert"></div>
 </div>
 <div class="row">
-    <div class="col-xs-4"></div>
-    <div class="col-xs-4">
+    <div class="col-xs-3"></div>
+    <div class="col-xs-6">
         <img src="<?php echo $poster['img'];?>" width="150" alt="海报图片">
     </div>
 </div>
 <hr>
 <?php foreach ($spread as $key=> $value){ ?>
 <div class="row">
-    <div class="col-xs-4"></div>
-    <div class="col-xs-4">
+    <div class="col-xs-3"></div>
+    <div class="col-xs-6">
         <div class="panel panel-default">
             <div class="panel-body">
                 <div class="media">
