f08a283c2233981cf71d4932287a69d261b83db0
chenbo <1264339504@qq.com>
2016-08-24 16:25:24 +0800

    database

--- application/views/guid_poster.php
+++ application/views/guid_poster.php
@@ -3,7 +3,7 @@
 <link href="/public/css/activity.css" rel="stylesheet" media="screen">
 <link href="/public/css/vip.css" rel="stylesheet" media="screen">
 <?php $url = ($poster_link['which'] === '1') ? $poster_link['weixin'] : $poster_link['link']; ?>
-<a href="<?php echo $url; ?>">
+<a href="<?php echo $url; ?>" target="_blank">
 <!--    <div style="width:100%;height:120px;background-image:url();background-repeat:no-repeat;background-size: auto">-->
 <!--    </div>-->
     <img src="<?php echo $poster['img'];?>" style="display: block;width: 100%;height: 120px">
